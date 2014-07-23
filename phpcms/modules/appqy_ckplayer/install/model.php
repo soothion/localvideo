0b0234847e20866aec2f963f40a8e62cb062b468
soothion <soothion@sina.com>
2014-07-23 09:07:25 +0800

    revemo ckplayer plugin

--- phpcms/modules/appqy_ckplayer/install/model.php
+++
@@ -1,5 +0,0 @@
-<?php 
-defined('IN_PHPCMS') or exit('Access Denied');
-defined('INSTALL') or exit('Accsess Denied');
-return array('appqy_ckplayer');
-?>
--- phpcms/modules/appqy_ckplayer/install/module.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `phpcms_module` 
-(`module`, `name`, `url`, `iscore`, `version`, `description`, `setting`, `listorder`, `disabled`, `installdate`, `updatedate`) 
-VALUES 
-('appqy_ckplayer', 'CkPlayer', 'appqy_ckplayer/', 0, '1.1', 'CkPlayer 6.X 模块配置  2013 Appqy.com ', '', 0, 0, '2013-06-01', '2013-06-01');
